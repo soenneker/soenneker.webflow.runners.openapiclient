24e744566dec47d25657c5d3471ed31e8f2747f4
soenneker <jake@soenneker.com>
2026-08-29 14:51:18 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Webflow.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
+++ src/Soenneker.Webflow.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
@@ -22,7 +22,7 @@ using System.Collections.Generic;
 
 namespace Soenneker.Webflow.Runners.OpenApiClient.Utils;
 
-///<inheritdoc cref="IFileOperationsUtil"/>
+/// <inheritdoc cref="IFileOperationsUtil"/>
 public sealed class FileOperationsUtil : IFileOperationsUtil
 {
     private readonly ILogger<FileOperationsUtil> _logger;
